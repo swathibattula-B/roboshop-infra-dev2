da3e9d05578eafacc39709d515c71678bba2c016
swathi <battulaswathi15407@gmail.com>
2026-07-08 13:39:19 +0530

    terraform

--- 00-vpc/outputs.tf
+++ 00-vpc/outputs.tf
@@ -3,13 +3,13 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-    value = module.vpc.public_subnet_ids
+    value = module.vpc.public_subnet
 }
 
 output "private_subnet_ids" {
-    value = module.vpc.private_subnet_ids
+    value = module.vpc.private_subnet
 }
 
 output "database_subnet_ids" {
-    value = module.vpc.database_subnet_ids
+    value = module.vpc.database_subnet
 }
