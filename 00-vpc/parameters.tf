b3fd76d61f0c3414458071308bed8f2f913b68c0
swathi <battulaswathi15407@gmail.com>
2026-07-08 14:05:40 +0530

    terraform

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -8,20 +8,20 @@ resource "aws_ssm_parameter" "vpc_id" {
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name        = "/${var.project}/${var.environment}/public_subnet_ids"
   type  = "StringList"
-  value = module.vpc.public_subnet
+  value = join(",", module.vpc.public_subnet)
 
 }
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name        = "/${var.project}/${var.environment}/private_subnet_ids"
   type  = "StringList"
-  value = module.vpc.private_subnet
+  value = join(",", module.vpc.private_subnet)
 
 }
 
 resource "aws_ssm_parameter" "database_subnet_ids" {
   name        = "/${var.project}/${var.environment}/database_subnet_ids"
   type  = "StringList"
-  value = module.vpc.database_subnet
+  value = join(",", module.vpc.database_subnet)
 
 }
