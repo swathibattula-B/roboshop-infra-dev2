d53349316101765b2d8abec430e35549991debd5
swathi <battulaswathi15407@gmail.com>
2026-07-09 00:36:15 +0530

    anisible

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -5,8 +5,8 @@ environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
-git clone https://github.com/swathibattula-B/-ansible-roboshop-roles-tf1.git
+git clone https://github.com/swathibattula-B/ansible-roboshop-roles-tf1.git
 
-cd -ansible-roboshop-roles-tf1
+cd ansible-roboshop-roles-tf1
 git pull
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
