10f4a6c27a82fd19ebd134fab87e2cb90b52a3e9
swathi <battulaswathi15407@gmail.com>
2026-07-09 22:24:38 +0530

    terraform

--- 40-databases/parameters.tf
+++ 40-databases/parameters.tf
@@ -1,5 +1,12 @@
+cat parameters.tf > /tmp/parameters_backup.tf && cat > parameters.tf << 'EOF'
+resource "random_password" "mysql_root" {
+  length  = 16
+  special = false
+}
+
 resource "aws_ssm_parameter" "mysql_root_password" {
   name  = "/${var.project}/${var.environment}/mysql_root_password"
   type  = "SecureString"
   value = random_password.mysql_root.result
 }
+EOF
