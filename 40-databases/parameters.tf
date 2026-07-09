73077a40247fc1ed8c20a3581aa01612f5d04edb
swathi <battulaswathi15407@gmail.com>
2026-07-09 22:56:15 +0530

    terraform

--- 40-databases/parameters.tf
+++ 40-databases/parameters.tf
@@ -7,4 +7,4 @@ resource "aws_ssm_parameter" "mysql_root_password" {
   name  = "/${var.project}/${var.environment}/mysql_root_password"
   type  = "SecureString"
   value = random_password.mysql_root.result
-}  
+}   
