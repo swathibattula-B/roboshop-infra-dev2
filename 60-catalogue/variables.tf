bc748ecf5356a1ce45846f2fb9edebc8d70f0ed7
swathi <battulaswathi15407@gmail.com>
2026-07-09 19:52:17 +0530

    terraform

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z077664125UVZ9NFX7BZC"
+}
+
+variable "domain_name" {
+    default = "daws88ss.online"
+}
+
+variable "app_version" {
+    default = "v3"
+}
